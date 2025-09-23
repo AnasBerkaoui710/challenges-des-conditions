edaab3f33ec9da212a6c1c427cd9f8fe0c7da041
Anas Berkaoui <berkaoui.anass12@gmail.com>
2025-09-23 17:49:18 +0100

    if condition

---
+++ challenge2.js
@@ -0,0 +1,9 @@
+let a = prompt("enter a number: ")
+
+if(a<0){
+    console.log("ce nombre est negatif")
+}else if(a>0){
+    console.log("ce nombre est positif")
+}else{
+    console.log("ce nombre est nul")
+}
